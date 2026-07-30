9f6e05ecaf44af9af3251254a39656cf3564fc69
Begad maged <eiadmaged1@gmail.com>
2026-07-30 13:36:08 +0300

    style(web): load calm navigation compression

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "./globals.css";
 import "./investigation-embedded.css";
+import "./calm-navigation.css";
 import { SessionProvider } from "./session";
 import { ThemeProvider } from "./theme";
 import { OFFICIAL_APP_DESCRIPTION, OFFICIAL_CLINIC_NAME } from "@/lib/brand";
