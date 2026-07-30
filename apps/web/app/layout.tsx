04044f981dd49aa63abe2c62832f2baafa58f2ed
Begad maged <eiadmaged1@gmail.com>
2026-07-30 14:11:34 +0300

    style(web): load calm directory finishing layer

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from "react";
 import "./globals.css";
 import "./investigation-embedded.css";
 import "./calm-navigation.css";
+import "./calm-directory.css";
 import { SessionProvider } from "./session";
 import { ThemeProvider } from "./theme";
 import { OFFICIAL_APP_DESCRIPTION, OFFICIAL_CLINIC_NAME } from "@/lib/brand";
