ed58540f42e45d1fbf7041b7043999a81d704de7
Begad maged <eiadmaged1@gmail.com>
2026-07-30 13:38:19 +0300

    feat(web): mount grouped visit navigation

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from "./theme";
 import { OFFICIAL_APP_DESCRIPTION, OFFICIAL_CLINIC_NAME } from "@/lib/brand";
 import { InterfaceModeProvider } from "@/lib/interface-mode";
 import { I18nProvider } from "@/i18n/useI18n";
+import { CalmVisitNavigation } from "@/components/clinic/CalmVisitNavigation";
 
 export const metadata: Metadata = {
   title: OFFICIAL_CLINIC_NAME,
@@ -23,7 +24,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <I18nProvider><ThemeProvider>
-          <SessionProvider><InterfaceModeProvider>{children}</InterfaceModeProvider></SessionProvider>
+          <SessionProvider><InterfaceModeProvider>
+            <CalmVisitNavigation />
+            {children}
+          </InterfaceModeProvider></SessionProvider>
         </ThemeProvider></I18nProvider>
       </body>
     </html>
